081cc50b5b92a7ea0b3b91bad73be33919d78632
WLM1ke <wlmike@gmail.com>
2023-04-02 14:05:57 +0300

    fix: actor signature compliant

--- poptimizer/core/telegram.py
+++ poptimizer/core/telegram.py
@@ -12,34 +12,35 @@ _MAX_TELEGRAM_MSG_SIZE: Final = 4096
 
 class Telegram:
     """Актор для отправки сообщений логера в Телеграм."""
-    def __init__(self, session: aiohttp.ClientSession, token: str, chat_id: str, level: int = logging.WARNING) -> None:
+
+    def __init__(self, client: aiohttp.ClientSession, token: str, chat_id: str, level: int = logging.WARNING) -> None:
         self._logger = logging.getLogger("Telegram")
-        self._session = session
+        self._client = client
         self._api_url = f"https://api.telegram.org/bot{token}/SendMessage"
         self._chat_id = chat_id
         self._fmt = "<strong>{name}</strong>\n{message}"
         self._level = level
 
-    async def __call__(self, ctx: actor.Ctx, record: logging.LogRecord) -> None:
+    async def __call__(self, ctx: actor.Ctx, msg: logging.LogRecord) -> None:
         """Посылает сообщения в телеграм с уровнем логирования начиная с заданного.
 
         Игнорирует сообщения от логера Telegram, чтобы избежать рекурсии на ошибках с отправкой.
 
         https://core.telegram.org/bots/api#sendmessage.
         """
-        if record.name == "Telegram":
+        if msg.name == "Telegram":
             return
 
-        msg = html.escape(record.msg) % record.args
+        message = html.escape(msg.msg) % msg.args
 
         json = {
             "chat_id": self._chat_id,
             "parse_mode": "HTML",
-            "text": self._fmt.format(name=record.name, message=msg)[:_MAX_TELEGRAM_MSG_SIZE],
+            "text": self._fmt.format(name=msg.name, message=message)[:_MAX_TELEGRAM_MSG_SIZE],
         }
 
-        if record.levelno >= self._level:
-            async with self._session.post(self._api_url, json=json) as resp:
+        if msg.levelno >= self._level:
+            async with self._client.post(self._api_url, json=json) as resp:
                 if not resp.ok:
                     err_desc = await resp.json()
                     self._logger.warning(f"can't send {err_desc}")
